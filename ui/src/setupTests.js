510676741d98e4b85b9284b4de9fe74fd6215a36
dwafotapa <dominique.wafotapa@gmail.com>
2017-11-25 16:26:18 +0100

    Remove obsolete conf

--- ui/src/setupTests.js
+++ ui/src/setupTests.js
@@ -3,7 +3,3 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
-
-global.requestAnimationFrame = function(callback) {
-  setTimeout(callback, 0);
-};
